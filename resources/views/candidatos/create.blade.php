7fb945bec39d06a790d96a56fab2227339c96452
Mauricio Stalcar <stalcarm@gmail.com>
2021-12-19 14:22:59 -0300

    modifiqué los formularios visualmente

--- resources/views/candidatos/create.blade.php
+++ resources/views/candidatos/create.blade.php
@@ -15,37 +15,38 @@
 <body>
 
     <form action="{{ url('/candidatos') }}" method="post" enctype="multipart/form-data">
-
+        <div class="container">
+        <div class="form-group">
         @csrf
 
         <label for="Nombre"> Nombre </label>
-        <input type="text" name="Nombre" id="Nombre">
+        <input class="form-control" type="text" name="Nombre" id="Nombre">
         <br>
 
         <label for="Apellido"> Apellido </label>
-        <input type="text" name="Apellido" id="Apellido">
+        <input class="form-control" type="text" name="Apellido" id="Apellido">
         <br>
 
         <label for="Telefono"> Telefono </label>
-        <input type="tel" name="Telefono" id="Telefono">
+        <input class="form-control" type="tel" name="Telefono" id="Telefono">
         <br>
 
         <label for="Correo"> Correo </label>
-        <input type="email" name="Correo" id="Correo">
+        <input class="form-control" type="email" name="Correo" id="Correo">
         <br>
 
-
         <label for="Foto"> Foto </label>
-        <input type="file" name="Foto" id="Foto">
+        <input class="form-control" type="file" name="Foto" id="Foto">
         <br>
 
         <label for="CV"> CV </label>
-         <input type="file" name="CV" id="CV">
+         <input class="form-control" type="file" name="CV" id="CV">
         <br>
 
-        <input type="submit" value="Guardar datos">
+        <input class="form-control" type="submit" value="Guardar datos">
         <br>
-
+    </div>
+    </div>
     </form>
 
 
--- resources/views/candidatos/edit.blade.php
+++ resources/views/candidatos/edit.blade.php
@@ -5,39 +5,40 @@
 <!--formulario de edicion de candidato-->
 
 <form action="{{ url('/candidatos/'.$candidatos->id) }}" method="post" enctype="multipart/form-data">
+
+    <div class="container">
+    <div class="form-group">
     @csrf
     {{ method_field('PATCH') }}
 
-    <label for="Nombre"> Nombre </label>
-        <input type="text" name="Nombre" value="{{$candidatos->Nombre}}" id="Nombre">
+        <label for="Nombre"> Nombre </label>
+        <input class="form-control" type="text" name="Nombre" value="{{$candidatos->Nombre}}" id="Nombre">
         <br>
-
         <label for="Apellido"> Apellido </label>
-        <input type="text" name="Apellido" value="{{$candidatos->Apellido}}" id="Apellido">
+        <input class="form-control" type="text" name="Apellido" value="{{$candidatos->Apellido}}" id="Apellido">
         <br>
 
         <label for="Telefono"> Telefono </label>
-        <input type="tel" name="Telefono" value="{{$candidatos->Telefono}}" id="Telefono">
+        <input class="form-control" type="tel" name="Telefono" value="{{$candidatos->Telefono}}" id="Telefono">
         <br>
 
         <label for="Correo"> Correo </label>
-        <input type="email" name="Correo" value="{{$candidatos->Correo}}" id="Correo">
+        <input class="form-control" type="email" name="Correo" value="{{$candidatos->Correo}}" id="Correo">
         <br>
 
-
         <label for="Foto"> Foto </label>
-        <input type="file" name="Foto" value="{{$candidatos->Foto}}" id="Foto">
+        <input class="form-control" type="file" name="Foto" value="{{$candidatos->Foto}}" id="Foto">
         <br>
 
         <label for="CV"> CV </label>
-         <input type="file" name="CV" value="{{$candidatos->CV}}" id="CV">
+         <input class="form-control" type="file" name="CV" value="{{$candidatos->CV}}" id="CV">
         <br>
 
-        <input type="submit" value="Guardar datos">
+        <input class="form-control" type="submit" value="Guardar datos">
         <br>
-
+    </div>
 </form>
-
+</div>
 <a href='{{ route('candidatos.index') }}'>Volver</a>
 
 @endsection
